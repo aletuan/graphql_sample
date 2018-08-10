7bfad80fe0293a6b7ba302ede621dd2d39cae932
Anh Le <anh.le@beteasy.com>
2018-08-10 13:22:20 +1000

    complex schema and query

--- server.js
+++ server.js
@@ -2,13 +2,78 @@ var express 		= require('express');
 var graphqlHTTP 	= require('express-graphql');
 var { buildSchema } = require('graphql');
 
+// note 1: input IDL code into schema
+// note 2: schema describe complete apis type system: a. data set, b. how access
+// note 3: each call validated with schema
+// note 4: data has structure, and strong type
+// note 5: putting exclam in the param type mean that it is required
 var schema = buildSchema(`
 	type Query {
-		message: String
+		course(id: Int!): Course
+		courses(topic: String!): [Course]
+	},
+	type Course {
+		id: Int
+		title: String
+		author: String
+		description: String
+		topic: String
+		url: String
 	}
 `);
 
-var root = { message: () => 'Anything else'};
+// define data
+const coursesData = [
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Node.js, Express & MongoDB Dev to Deployment',
+        author: 'Brad Traversy',
+        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    }
+];
+
+// implement query function
+const getCourse = (args) => {
+	let id = args.id;
+	return coursesData.filter(course => {
+		return course.id === id;
+	})[0];
+}
+
+const getCourses = (args) => {
+	if (args.topic) {
+		let topic = args.topic;
+		return coursesData.filter(course => course.topic === topic);
+	} else {
+		// return all if no topic input ?
+		return coursesData;
+	}
+}
+
+// root resolver
+// note 1: mapping actions to functions. in here, action is message
+var root = { 
+	course: getCourse,
+	courses: getCourses
+};
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -16,6 +81,38 @@ app.use('/graphql', graphqlHTTP({
 	rootValue: root,
 	graphiql: true,
 }));
+
+// Sample 1, get single course
+// {
+//   course(id: 1) {
+//     title
+//     author
+//   }
+// }
+// Sample 2, get course list
+// {
+//   courses(topic: "Node.js") {
+//     title,
+//     author,
+//     description,
+//     topic,
+//     url,
+//   }
+// }
+// Sample 3, can write query function
+// query getMultiCourse($courseTopic: String!) {
+//   courses(topic: $courseTopic) {
+//     title
+//     author
+//     description
+//     topic
+//     url
+//   }
+// }
+// with query variable as input
+// {
+//   "courseTopic": "Node.js"
+// }
 
 app.listen(4000, () => console.log('Up in port 4000/graphql'));
 
